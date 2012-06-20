a8c720b79b066ed6c867af8fa6600b856bbec67d
Peter Braden <peterbraden@peterbraden.co.uk>
2012-06-20 17:53:52 -0400

    add real value to holdings

--- reports/holdings.js
+++ reports/holdings.js
@@ -5,15 +5,16 @@ var Table = require('cli-table')
 var COLS = {
     asset : {title : "Asset", ind : 0}
   , cbval : {title : "CB Val", ind : 1}
-  , cbprop: {title : "CB % Equity", ind: 2}
+  , val : {title: "Curr Val", ind: 2}
+  //, cbprop: {title : "CB % Equity", ind: 2}
   , cbpropnet: {title : "CB % Net", ind: 3}
+  , valpropnet: {title : "Val % Net", ind: 4}
 }  
   
 module.exports = function(opts){
   return {
     onComplete: function(banks, stocks){
-     
-      var cols = ["Asset", "CB Val"]
+      ac.loadPrices(stocks, function(stocks){
 
       var t = new Table({
           head : _.map(COLS, function(v, k){return v.title})
@@ -25,17 +26,21 @@ module.exports = function(opts){
         , net_worth = 0
         , net_equity = 0
         , cash = 0
+        , total_worth = 0
 
       _.each(banks, function(b, k){
         if (!b.balance || !b.last_statement) return;
 
         cash += b.balance
         net_worth += b.balance
+        total_worth += b.balance
       })
 
       assets.push({
           symbol: "Cash".yellow
+        , current: 1
         , cost_basis: cash
+        , quantity: cash
       })
 
       _.each(stocks, function(s, symbol){
@@ -44,6 +49,7 @@ module.exports = function(opts){
         assets.push(s);
         net_worth += s.cost_basis
         net_equity += s.cost_basis
+        total_worth += s.current * s.quantity
 
       })
 
@@ -54,12 +60,16 @@ module.exports = function(opts){
       _.each(assets, function(x){
         t.push([x.symbol
           , ac.c(x.cost_basis)
-          , x.equity ? ac.c(x.cost_basis/net_equity*100) : '-'
-          , ac.c(x.cost_basis/net_worth*100)])
+          //, x.equity ? ac.c(x.cost_basis/net_equity*100) : '-'
+          , ac.c(x.current * x.quantity)
+          , ac.c(x.cost_basis/net_worth*100)
+          , ac.c(x.current*x.quantity/total_worth*100)
+          ])
       })
 
       console.log(t.toString());
 
+    })
     }
   }
 }
