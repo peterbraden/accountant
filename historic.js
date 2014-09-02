bcb7b403ac7ef3a514a094270aad449a5e5fdb45
asadmalik <asadmalik.pk@gmail.com>
2014-09-03 00:54:03 +0500

    Update historic.js

--- historic.js
+++ historic.js
@@ -5,7 +5,7 @@ var request = require('request')
   , path = require('path')
 
 var HIST_URL = "http://www.google.com/finance/historical?q={{ stock }}&output=csv"
-  , MONTHS = {Jan:'01',Feb:'02',Mar:'03',Apr:'04',May:'05',Jun:'06',Jul:'07',Aug:'08',Sep:'09',Oct:'10',Nov:11,Dec:12}
+  , MONTHS = {Jan:'01',Feb:'02',Mar:'03',Apr:'04',May:'05',Jun:'06',Jul:'07',Aug:'08',Sep:'09',Oct:'10',Nov:'11',Dec:'12'}
   , CACHE_PATH = 'hist-stock-data.json'
 
 
